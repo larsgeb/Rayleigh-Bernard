4bdcf0f15922ff115642d4453789dd58323d4299
Lars Gebraad <larsgebraad@gmail.com>
2018-03-20 14:06:38 +0100

    Forwarded the main script a little.

--- alt_main.py
+++ alt_main.py
@@ -5,18 +5,21 @@ import scipy.sparse as sparse
 import discretizationUtils as grid
 import config as config
 import operators as op
+import matrices
 
 # --- Setup --- #
 config.ny = 6
 config.nx = config.ny * 2
 config.dy = 1
 config.dx = 1
+# config.dy = 1/(config.ny-1)
+# config.dx = 1/(config.ny-1)
 ny = config.ny
 nx = config.nx
 dy = config.dy
 dx = config.dx
 
-sqrt_ra = 7
+sqrtRa = 7
 Tbottom = 1
 Ttop = 0
 # ---  end  --- #
@@ -27,11 +30,16 @@ startT = np.expand_dims(grid.fieldToVec(startT), 1)
 startPsi = np.expand_dims(np.zeros(ny), 1).repeat(nx, axis=1)
 startPsi = np.expand_dims(grid.fieldToVec(startPsi), 1)
 
-dyOp = op.dyOp(nx,ny)
-dxOp = op.dxOp(nx,ny)
-dlOp = op.dlOpStream(nx,ny)
+# Generate operators for differentials (generating them in functions is more computationally expensive)
+dyOpPsi = op.dyOp(nx, ny)
+dxOpPsi = op.dxOpStream(nx, ny)
+dlOpPsi = op.dlOpStream(nx, ny)
+dlOpTemp, rhsDlOpTemp = op.dlOpTemp(nx, ny, [Tbottom, Ttop], [0, 0])
+dxOpTemp, rhsDxOpTemp = op.dxOpTemp(nx, ny, [0, 0])
+dyOpTemp, rhsDyOpTemp = op.dyOpTemp(nx, ny, [Tbottom, Ttop])
 
-# print(dyOp)
-plt.imshow(dlOp.todense())
-plt.colorbar()
-plt.show()
+
+C, rhsC = matrices.constructC(dxOpTemp=dxOpTemp, rhsDxOpTemp=rhsDxOpTemp, dyOpTemp=dyOpTemp, rhsDyOpTemp=rhsDyOpTemp,
+                              psi=startPsi, dxOpPsi=dxOpPsi, dyOpPsi=dyOpPsi, sqrtRa=sqrtRa)
+
+print(rhsC)
